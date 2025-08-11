868222f117e8ae69f413a5e952bff66cbc7b0ecc
MMike17 <matthews.r.mike@gmail.com>
2025-08-11 17:40:30 +0200

    Add basic rain settings

--- Patches.cs
+++ Patches.cs
@@ -1,27 +1,62 @@
 using HarmonyLib;
+using UnityEngine;
+
+using static UnityEngine.ParticleSystem;
 
 namespace HeavyRain
 {
-	// Patch model
-	// [HarmonyPatch(typeof(), nameof())]
-	// [HarmonyPatch(typeof(), MethodType.)]
-	// static class type_method_Patch
-	// {
-	// 	static void Prefix()
-	// 	{
-	// 		//
-	// 	}
-	
-	//	this will negate the method
-	//  	static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-	//  	{
-	//      	foreach (var instruction in instructions)
-	//          	yield return new CodeInstruction(OpCodes.Ret);
-	//  	}
+    // Patch model
+    // [HarmonyPatch(typeof(), nameof())]
+    // [HarmonyPatch(typeof(), MethodType.)]
+    // static class type_method_Patch
+    // {
+    // 	static void Prefix()
+    // 	{
+    // 		//
+    // 	}
 
-	// 	static void Postfix()
-	// 	{
-	// 		//
-	// 	}
-	// }
+    //	this will negate the method
+    //  	static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    //  	{
+    //      	foreach (var instruction in instructions)
+    //          	yield return new CodeInstruction(OpCodes.Ret);
+    //  	}
+
+    // 	static void Postfix()
+    // 	{
+    // 		//
+    // 	}
+    // }
+
+    [HarmonyPatch(typeof(EventManager), MethodType.Constructor)]
+    static class RainSetter
+    {
+        const int defaultRainEmission = 1000;
+        const int defaultRainLimit = 500;
+
+        private static ParticleSystem rainVFX;
+
+        public static void SetRain()
+        {
+            if (!Main.enabled)
+                return;
+
+            EmissionModule emission = rainVFX.emission;
+            MainModule main = rainVFX.main;
+
+            int multiplier = (int)Main.settings.rainMultiplier;
+            emission.rateOverTime = defaultRainEmission * multiplier;
+            main.maxParticles = defaultRainLimit * multiplier;
+
+            Color color = main.startColor.color;
+            color.a = Main.settings.rainAlpha;
+            main.startColor = new MinMaxGradient(color);
+        }
+
+        static void Postfix()
+        {
+            rainVFX = GameObject.Find("RainParticles").GetComponent<ParticleSystem>();
+            Main.Log("Found rain VFX : " + (rainVFX != null));
+        }
+    }
 }
--- Settings.cs
+++ Settings.cs
@@ -7,20 +7,37 @@ namespace HeavyRain
 {
     public class Settings : ModSettings, IDrawable
     {
+        readonly static float defaultRainAlpha = 0.372549f;
+
+        public enum Multiplier
+        {
+            x1 = 1,
+            x10 = 10,
+            x100 = 100
+        }
+
         // [Draw(DrawType.)]
-        
+
+        [Header("Rain")]
+        [Draw(DrawType.Auto)]
+        public Multiplier rainMultiplier = Multiplier.x10;
+        [Draw(DrawType.Slider, Min = 0, Max = 1)]
+        public float rainAlpha = 0.15f;
+
         [Header("Debug")]
         [Draw(DrawType.Toggle)]
         public bool showMarkers;
         [Draw(DrawType.Toggle)]
-        public bool disableInfoLogs = true;
+        public bool disableInfoLogs = false;
+        //public bool disableInfoLogs = true;
 
         public override void Save(ModEntry modEntry) => Save(this, modEntry);
 
         public void OnChange()
         {
-            Main.SetMarkers(showMarkers);
+            RainSetter.SetRain();
 
+            //Main.SetMarkers(showMarkers);
             // SnapValue(, 0.1f);
         }
 
@@ -30,6 +47,17 @@ namespace HeavyRain
             float minTarget = snapValue - snapDiff / 2;
             float maxTarget = snapValue + snapDiff / 2;
             return value <= maxTarget && value >= minTarget ? snapValue : value;
+        }
+
+        internal void OnGUI()
+        {
+            if (GUILayout.Button("Reset rain settings"))
+            {
+                rainMultiplier = Multiplier.x1;
+                rainAlpha = defaultRainAlpha;
+
+                RainSetter.SetRain();
+            }
         }
     }
 }
