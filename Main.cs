800eddbc5ca56475435bf31d712151bc83439fc9
MMike17 <matthews.r.mike@gmail.com>
2025-08-12 20:20:05 +0200

    Remove markers

--- Main.cs
+++ Main.cs
@@ -1,8 +1,6 @@
 using HarmonyLib;
 using System;
-using System.Collections.Generic;
 using System.Reflection;
-using UnityEngine;
 using UnityModManagerNet;
 
 using static UnityModManagerNet.UnityModManager;
@@ -19,9 +17,6 @@ namespace HeavyRain
         public static Settings settings;
         /// <summary>This will be called when the mod is toggles on/off</summary>
         public static event Action<bool> OnToggle;
-
-        static List<GameObject> markers;
-        static Material markerMat;
 
         // Called by the mod manager
         static bool Load(ModEntry modEntry)
@@ -58,7 +53,6 @@ namespace HeavyRain
                 }
             };
 
-            markers = new List<GameObject>();
             return true;
         }
 
@@ -132,46 +126,6 @@ namespace HeavyRain
             }
 
             info.Invoke(source, args);
-        }
-
-        public static void SetMarkers(bool state)
-        {
-            CleanMarkerList();
-            markers.ForEach(item => item.SetActive(state));
-        }
-
-        public static void AddMarker(Transform parent, Vector3 position, float size)
-        {
-            if (markerMat == null)
-            {
-                markerMat = new Material(Shader.Find("Standard"));
-                markerMat.color = Color.red;
-                markerMat.SetColor("_EmissionColor", Color.red);
-            }
-
-            GameObject marker = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            marker.GetComponent<Renderer>().material = markerMat;
-
-            marker.transform.SetParent(parent);
-            marker.transform.position = position;
-            marker.transform.localScale = Vector3.one * size;
-
-            marker.SetActive(settings.showMarkers);
-            markers.Add(marker);
-        }
-
-        static void CleanMarkerList()
-        {
-            List<int> toRemove = new List<int>();
-
-            for (int i = 0; i < markers.Count; i++)
-            {
-                if (markers[i] == null)
-                    toRemove.Add(i);
-            }
-
-            toRemove.Reverse();
-            toRemove.ForEach(index => markers.RemoveAt(index));
         }
     }
 }
